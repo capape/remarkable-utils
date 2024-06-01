28312d80da0d4f555f299604de46486ef18aa90b
Jose Antonio Capape Gil <capapegil@gmail.com>
2024-06-01 10:44:45 +0200

    Files to restore

---
+++ after-update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp suspended.png /usr/share/remarkable/ 
+
+cd /home/root/templates
+cp -r astronomy/ /usr/share/remarkable/templates/
+cp templates-withastro.json /usr/share/remarkable/templates/templates.json
